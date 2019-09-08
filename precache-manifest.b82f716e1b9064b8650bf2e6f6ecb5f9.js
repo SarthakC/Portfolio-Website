cf1753012f5a740a714875ce18c97e7937c4001a
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-09 01:51:57 +0530

    Updates

---
+++ precache-manifest.b82f716e1b9064b8650bf2e6f6ecb5f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2fb45a074463d471e6dcdd8d3524fd0",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "c2ab09a69b99283936da",
+    "url": "/Portfolio-Website/static/css/main.f5ad7dbc.chunk.css"
+  },
+  {
+    "revision": "c9bcaa3bf9d4126d66df",
+    "url": "/Portfolio-Website/static/js/2.4eb9986e.chunk.js"
+  },
+  {
+    "revision": "c2ab09a69b99283936da",
+    "url": "/Portfolio-Website/static/js/main.9c6e44f4.chunk.js"
+  },
+  {
+    "revision": "d7de11a01bd7919ffa49",
+    "url": "/Portfolio-Website/static/js/runtime~main.d511aa2e.js"
+  }
+]);
